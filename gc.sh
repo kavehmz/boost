cfd356fbec142fddc93917eee380eb8c8c470fa5
Kaveh Mousavi Zamani <k.mousavizamani@travelaudience.com>
2017-07-19 10:47:05 +0200

    adding k8s shortcuts

--- gc.sh
+++ gc.sh
@@ -14,18 +14,34 @@ case "$1" in
   "ssh")
     find_and_ssh $2
     ;;
+
+  "cls")
+    echo "list of containers in [$CLOUDSDK_CORE_PROJECT]"
+    gcloud container clusters list|cat -n
+    ;;
+  "csel")
+    find_and_set_k8s $2
+    ;;
   *)
+    echo "Note: [Identifier] is a number or name in the list"
     echo "Usage:"
     echo "gc ls # list projects"
-    echo "gc sel [Identifier] # select a project name/number"
-    echo "gc ils # list instances"
-    echo "gc ssh [Identifier] # name or number"
+    echo "gc sel [Identifier] # select a project"
+    echo "gc ils # list of instances"
+    echo "gc ssh [Identifier]"
+    echo "gc cls # list containers"
+    echo "gc csel [Identifier] # get credentials for a container"
     ;;
 esac
-
 
 function find_and_ssh {
     local SERVER=$(gcloud compute instances list|cat -n|egrep "\s+$1\s"|perl -e '$l=<>;chomp $l;$l=~ s/^\s+\d+\s(.*?)\s+(.*?)\s+.*/$1 --zone $2/g;print $l,"\n"')
     echo "ssh into [kmousavizamani@$SERVER]"
     gcloud compute ssh kmousavizamani@$SERVER
+}
+
+function find_and_set_k8s {
+    local CONTAINER=$(gcloud container clusters list|cat -n|egrep "\s+$1\s"|perl -e '$l=<>;chomp $l;$l=~ s/^\s+\d+\s(.*?)\s+(.*?)\s+.*/$1 --zone $2/g;print $l,"\n"')
+    echo "clusters get-credentials for [$CONTAINER]"
+    gcloud container clusters get-credentials $CONTAINER
 }
