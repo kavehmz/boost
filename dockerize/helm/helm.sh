527fbb06205cd4988a3e128c75c9fcccb77f9c67
Kaveh Mousavi Zamani <kmousavizamani@wayfair.com>
2019-10-24 16:40:59 +0200

    keep the bash history even inside docker

--- dockerize/helm/helm.sh
+++ dockerize/helm/helm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -ti --rm -v $HOME:$HOME -v $HOME/.helm:/root/.helm -v $HOME/.kube:/root/.kube --workdir $PWD --entrypoint /bin/bash local-helm:2.13.1 "$@"
+docker run -ti --rm -v $HOME:$HOME -v $HOME/.helm:/root/.helm -v $HOME/.kube:/root/.kube -v $HOME/.bash_history:/root/.bash_history --workdir $PWD --entrypoint /bin/bash local-helm:2.13.1 "$@"
